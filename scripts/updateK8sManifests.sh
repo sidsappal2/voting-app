80fb8a55b89f9f92dd67c4906eb0c0be86a31448
Siddharth Sappal <sidsappal@gmail.com>
2024-03-01 11:40:13 +0000

    Updated updateK8sManifests.sh

--- scripts/updateK8sManifests.sh
+++ scripts/updateK8sManifests.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
+
 set -x
+
 # Set the repository URL
-REPO_URL="https://hbjg66nhsex6nketgastp4kiz5xl7udaxalhjlqllednybzbmgsa@dev.azure.com/sidsappal/voting-app/_git/voting-app"
+REPO_URL="https://6ajd6oeawsl43dzktsqidu2asrnssp7n2oivo3a475ycwolp4rpq@dev.azure.com/sidsappal/voting-app/_git/voting-app"
 
 # Clone the git repository into the /tmp directory
-git clone "$REPO_URL"/tmp/temp_repo
+git clone "$REPO_URL" /tmp/temp_repo
 
 # Navigate into the cloned repository directory
 cd /tmp/temp_repo
+pwd
 
+echo $1
+echo $2
+echo $3
 # Make changes to the Kubernetes manifest file(s)
 # For example, let's say you want to change the image tag in a deployment.yaml file
-sed -i "s|image:*|image: sidazurecicd.azurecr.io/$2:$3|g" k8s-specifications/$1-deployment.yaml
+sed -i "s|image:.*|image: sidazurecicd.azurecr.io/$2:$3|g" k8s-specifications/$1-deployment.yaml
 
 # Add the modified files
 git add .
 
 # Commit the changes
-git commit -m "Update Kubernetes manifest $2:$3"
+git commit -m "Update Kubernetes manifest"
 
 # Push the changes back to the repository
 git push
